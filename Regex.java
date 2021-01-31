80e837e6fbbb12d83321372b59f04db4ba93ec99
unknown <patilsharad2000@gmail.com>
2021-01-31 17:20:04 +0530

    Get User First Name Validation

--- Regex.java
+++ Regex.java
@@ -1,5 +1,27 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 class Regex {
-	public static void main(String[] args){
-		System.out.println("Welcome to User Registration");
+
+	public void firstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String firstName = sc.next();
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		boolean result = matcher.matches();
+		if(result == true)
+			System.out.println("First Name is Valid...");
+		else
+			System.out.println("First Name is not valid...");
 	}
+
+	public static void main(String[] args) {
+		Regex regex = new Regex();
+		regex.firstName();
+	}
+
 }
